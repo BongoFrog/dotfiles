af6a74b5c0de6d75a5ac960316286c6e2986542d
BongoFrog <hlongcoder@gmail.com>
2026-07-10 14:59:13 +0700

    add config: modules

--- dot_config/hypr/hyprland.lua
+++ dot_config/hypr/hyprland.lua
@@ -1,2 +1,3 @@
 require("modules.binds")
 require("modules.monitor")
+require("modules.autostart")
---
+++ dot_config/hypr/modules/autostart.lua
@@ -0,0 +1,3 @@
+hl.on("hyprland.start", function () 
+   hl.exec_cmd("qs -p ~/.config/quickshell/mainTaskbar.qml")
+ end)
